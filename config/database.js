2c938d3410a90d54a3d5207f9f4032b9c25cd69b
jbeltrami <joao_beltrami@hotmail.com>
2017-05-22 17:20:17 -0400

    Changes the express-api-template references
    Updates all template references and point all the info to fileBucket.
    References include:
    - mongo table name
    - error handler file
    - users debug function pointing to the correct db
    - root title

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const mongooseBaseName = 'express-api-template';
+const mongooseBaseName = 'fileBucket';
 
 const environment = process.env.NODE_ENV || 'development';
 const database = {
